636acce8a5bb75ed11a6c63fcb84b1b5389cc895
sugakandrey <2th3sk13s@gmail.com>
2014-09-20 22:49:14 +0400

    rotation implemented

--- app/src/main/java/ru/ifmo/md/lesson2/MyActivity.java
+++ app/src/main/java/ru/ifmo/md/lesson2/MyActivity.java
@@ -8,17 +8,17 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.*;
 
 public class MyActivity extends Activity {
     private ImageView view;
-    public static final float scale = 1.73f;
-    private int INITIAL_WIDTH = 750;
-    private int INITIAL_HEIGHT = 700;
-    private int[] pixels = null;
-    private int[] brightPixels = null;
-    private int NEW_WIDTH = 434;
-    private int NEW_HEIGHT = 405;
+    private final int INITIAL_WIDTH = 750;
+    private final int INITIAL_HEIGHT = 700;
+    private final int NEW_HEIGHT = 405;
+    private final int NEW_WIDTH = 434;
+    public final float scale = INITIAL_WIDTH / NEW_WIDTH;
+    private int[] pixels = new int[INITIAL_WIDTH * INITIAL_HEIGHT];
+    private int[] rotated = new int[INITIAL_HEIGHT * INITIAL_WIDTH];
+    private int[] brightPixels = new int[INITIAL_HEIGHT * INITIAL_WIDTH];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,18 +30,24 @@ public class MyActivity extends Activity {
 
     private void reconfigureImage() {
         Bitmap image = BitmapFactory.decodeResource(this.getResources(), R.drawable.source);
-
-        pixels = new int[INITIAL_WIDTH * INITIAL_HEIGHT];
-        brightPixels = new int[INITIAL_WIDTH * INITIAL_HEIGHT];
-
         image.getPixels(pixels, 0, INITIAL_WIDTH, 0, 0, INITIAL_WIDTH, INITIAL_HEIGHT);
-
+        shrinkAndRotate();
         brighten();
+    }
+
+
+    private void shrinkAndRotate() {
+        int index = 0;
+        for (int i = 0; i < INITIAL_WIDTH; i++) {
+            for (int j = INITIAL_HEIGHT - 1; j >= 0; j--) {
+                rotated[index++] = pixels[i + j * INITIAL_WIDTH];
+            }
+        }
     }
 
     private void brighten() {
         for (int i = 0; i < INITIAL_WIDTH * INITIAL_HEIGHT; i++) {
-            int pixel = pixels[i];
+            int pixel = rotated[i];
             brightPixels[i] = convertColor(pixel);
         }
     }
@@ -55,7 +61,6 @@ public class MyActivity extends Activity {
 
     private class ImageView extends View {
         private boolean isFast;
-
 
         public ImageView(Context context) {
             super(context);
@@ -71,12 +76,10 @@ public class MyActivity extends Activity {
 
         @Override
         protected void onDraw(Canvas canvas) {
-            canvas.save();
             if (isFast)
-                canvas.drawBitmap(pixels, 0, INITIAL_WIDTH, 0, 0, INITIAL_WIDTH, INITIAL_HEIGHT, false, null);
+                canvas.drawBitmap(rotated, 0, INITIAL_HEIGHT, 0, 0, INITIAL_HEIGHT, INITIAL_WIDTH, false, null);
             else
-                canvas.drawBitmap(brightPixels,0, INITIAL_WIDTH, 0, 0, INITIAL_WIDTH, INITIAL_HEIGHT, false, null );
-            canvas.restore();
+                canvas.drawBitmap(brightPixels, 0, INITIAL_HEIGHT, 0, 0, INITIAL_HEIGHT, INITIAL_WIDTH, false, null);
         }
     }
 }
