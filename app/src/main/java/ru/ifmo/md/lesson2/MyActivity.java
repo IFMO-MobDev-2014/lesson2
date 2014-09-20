8fdd5e3ef0f52b7734c2afebe85a69d398bccdd2
pinguinson <nikita.penguin@gmail.com>
2014-09-20 19:31:15 +0400

    now i can switch between images by clicking on them

--- app/src/main/java/ru/ifmo/md/lesson2/MyActivity.java
+++ app/src/main/java/ru/ifmo/md/lesson2/MyActivity.java
@@ -2,11 +2,31 @@ package ru.ifmo.md.lesson2;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
 
 public class MyActivity extends Activity {
 
+    ImageView image;
+    boolean setFast;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        setFast = true;
+
+        image = (ImageView) findViewById(R.id.image);
     }
+
+    public void changeImage(View view) {
+        if (setFast) {
+            image.setImageResource(R.drawable.source2);
+        } else {
+            image.setImageResource(R.drawable.source);
+        }
+        setFast = !setFast;
+    }
+
 }
