22db84129949f7506cd8c56e348c0fc712a0e8e6
Kuznetsov Nikolay <kna.rus@gmail.com>
2014-09-20 19:09:48 +0400

    Added AsyncTask, minor cleanups, fixed rotation, improved caching, code autoformat

--- app/src/main/java/ru/ifmo/md/lesson2/MyActivity.java
+++ app/src/main/java/ru/ifmo/md/lesson2/MyActivity.java
@@ -3,17 +3,32 @@ package ru.ifmo.md.lesson2;
 import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.Color;
-import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
 
-import java.util.Arrays;
-
 public class MyActivity extends Activity {
+
+    // a task so that UI thread doesn't have the 200ms lag associated with slow rescaling
+    private class AsyncResizeTask extends AsyncTask<Boolean, Void, Bitmap> {
+        @Override
+        public Bitmap doInBackground(Boolean[] params) {
+            Bitmap rv = getAdjustedImage(params[0]);
+            if (params[0])
+                fastImage = rv;
+            else
+                slowImage = rv;
+            return rv;
+        }
+
+        @Override
+        public void onPostExecute(Bitmap bm) {
+            view.setImageBitmap(bm);
+            art = null;
+        }
+    }
 
     private ImageView view;
 
@@ -23,51 +38,49 @@ public class MyActivity extends Activity {
     boolean useFast = false;
 
     static int[] pixelTransformTable = new int[256];
+
     static {
         for (int i = 0; i < 256; i++) { // offers subjectively better quality
-            pixelTransformTable[i] = (int)(Math.sqrt(((float)i)/255.0f)*255.0f);
+            pixelTransformTable[i] = (int) (Math.sqrt(((float) i) / 255.0f) * 255.0f);
         }
     }
 
     Bitmap sourceBitmap = null;
     int[] oldPixels = null;
-    int[] newPixels = null;
-    int[] scratchpad = null;
+    Bitmap fastImage = null;
+    Bitmap slowImage = null;
 
-    private Bitmap getAdjustedImage(int w, int h, boolean fast) {
-        if(sourceBitmap == null)
+    AsyncResizeTask art = null;
+
+    private Bitmap getAdjustedImage(boolean fast) {
+        int w = TARGET_WIDTH;
+        int h = TARGET_HEIGHT;
+        if (sourceBitmap == null)
             sourceBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.source);
         int oldW = sourceBitmap.getWidth();
         int oldH = sourceBitmap.getHeight();
-        if(oldPixels == null)
+        if (oldPixels == null) {
             oldPixels = new int[oldW * oldH];
+            sourceBitmap.getPixels(oldPixels, 0, oldW, 0, 0, oldW, oldH);
+        }
 
-        sourceBitmap.getPixels(oldPixels, 0, oldW, 0, 0, oldW, oldH);
-        if(newPixels == null)
-            newPixels = new int[w * h];
-
+        int[] newPixels = new int[w * h];
         int[] oldPixels = this.oldPixels;
-        int[] newPixels = this.newPixels;
 
         long startTime = System.nanoTime();
-        if(fast) { // fast method: nearest
-            for(int y = 0; y < h; y++) {
-                for(int x = 0; x < w; x++) {
-                    newPixels[x + y * w] = oldPixels[y * oldW / h + x * oldH / w * oldW];
+        if (fast) { // fast method: nearest
+            for (int y = 0; y < h; y++) {
+                for (int x = 0; x < w; x++) {
+                    newPixels[x + y * w] = oldPixels[(h - 1 - y) * oldW / h + x * oldH / w * oldW];
                 }
             }
         } else { // slow method: plain 'supersampling'
-            if(scratchpad == null)
-                scratchpad = new int[w * h];
-            else
-                Arrays.fill(scratchpad, 0);
+            int[] scratchpad = new int[w * h];
 
-            int[] scratchpad = this.scratchpad;
-
-            for(int x = 0; x < oldW; x++)
-                for(int y = 0; y < oldH; y++) {
+            for (int x = 0; x < oldW; x++)
+                for (int y = 0; y < oldH; y++) {
                     int targetX = y * w / oldH;
-                    int targetY = x * h / oldW;
+                    int targetY = h - 1 - x * h / oldW;
                     int targetIdx = targetX + targetY * w;
 
                     int cpv = newPixels[targetIdx];
@@ -85,10 +98,10 @@ public class MyActivity extends Activity {
                     newPixels[targetIdx] = 0xff000000 | (b << 16) | (g << 8) | r;
                 }
         }
-        Log.d("TIME", "Rescale: " + (System.nanoTime() - startTime) / 1000000 );
+        Log.d("TIME", "Rescale: " + (System.nanoTime() - startTime) / 1000000);
         startTime = System.nanoTime();
         int[] pixelTransformTable = MyActivity.pixelTransformTable;
-        for(int i = 0; i < newPixels.length; i++) {
+        for (int i = 0; i < newPixels.length; i++) {
             int r = newPixels[i] & 0xff;
             int g = (newPixels[i] & 0xff00) >> 8;
             int b = (newPixels[i] & 0xff0000) >> 16;
@@ -97,17 +110,29 @@ public class MyActivity extends Activity {
             b = pixelTransformTable[b];
             newPixels[i] = 0xff000000 | (b << 16) | (g << 8) | r;
         }
-        Log.d("TIME", "Recolor: " + (System.nanoTime() - startTime) / 1000000 );
+        Log.d("TIME", "Recolor: " + (System.nanoTime() - startTime) / 1000000);
         return Bitmap.createBitmap(newPixels, w, h, Bitmap.Config.ARGB_8888);
     }
 
     @Override
     public void onTrimMemory(int level) {
-        scratchpad = null;
-        newPixels = null;
         oldPixels = null;
-        if(level >= TRIM_MEMORY_UI_HIDDEN)
-            sourceBitmap = null;
+        sourceBitmap = null;
+        if (level >= TRIM_MEMORY_UI_HIDDEN) {
+            fastImage = null;
+            slowImage = null;
+        }
+    }
+
+    private void processPicture() {
+        if (useFast && fastImage != null)
+            view.setImageBitmap(fastImage);
+        else if (!useFast && slowImage != null)
+            view.setImageBitmap(slowImage);
+        else if (art == null) {
+            art = new AsyncResizeTask();
+            art.execute(useFast);
+        }
     }
 
     @Override
@@ -115,11 +140,11 @@ public class MyActivity extends Activity {
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.layout);
         view = (ImageView) findViewById(R.id.imageView);
-        view.setImageBitmap(getAdjustedImage(TARGET_WIDTH, TARGET_HEIGHT, useFast));
+        processPicture();
     }
 
     public void onClick(View v) {
         useFast = !useFast;
-        view.setImageBitmap(getAdjustedImage(TARGET_WIDTH, TARGET_HEIGHT, useFast));
+        processPicture();
     }
 }
