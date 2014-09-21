c502af7918980087b47843ea499e8f724de8b06c
NoNick <aleksand634@gmail.com>
2014-09-21 22:59:23 +0000

    Multithreaded fast resize (40-80 ms)

--- app/src/main/java/ru/ifmo/md/lesson2/ImageMagic.java
+++ app/src/main/java/ru/ifmo/md/lesson2/ImageMagic.java
@@ -8,6 +8,7 @@ import android.support.v8.renderscript.RenderScript;
 import android.support.v8.renderscript.Allocation;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.widget.Toast;
 
 public class ImageMagic extends SurfaceView implements Runnable{
     SurfaceHolder holder;
@@ -29,10 +30,10 @@ public class ImageMagic extends SurfaceView implements Runnable{
         initImage();
     }
 
-    private void initImage() {
-        Bitmap bright = Bitmap.createBitmap(img.getWidth(), img.getHeight(), img.getConfig());
+    private Bitmap increaseBrightness(Bitmap bitmap) {
+        Bitmap bright = Bitmap.createBitmap(bitmap.getWidth(), bitmap.getHeight(), bitmap.getConfig());
         RenderScript brightRs = RenderScript.create(context);
-        Allocation inAlloc = Allocation.createFromBitmap(brightRs, img);
+        Allocation inAlloc = Allocation.createFromBitmap(brightRs, bitmap);
         Allocation outAlloc = Allocation.createTyped(brightRs, inAlloc.getType());
         ScriptC_incBright brightScript = new ScriptC_incBright(brightRs);
         brightScript.set_outAlloc(outAlloc);
@@ -40,7 +41,16 @@ public class ImageMagic extends SurfaceView implements Runnable{
         brightScript.set_gScript(brightScript);
         brightScript.invoke_filter();
         outAlloc.copyTo(bright);
-        img = bright;
+        return bright;
+    }
+
+    private void initImage() {
+//        long timeStart = System.currentTimeMillis();
+        img = reduceImageFast(img, 405, 434);
+//        long timeEnd = System.currentTimeMillis();
+//        Toast t = Toast.makeText(context, Integer.toString((int) (timeEnd - timeStart)), Toast.LENGTH_SHORT);
+//        t.show();
+        img = increaseBrightness(img);
     }
 
     public void resume() {
@@ -69,5 +79,47 @@ public class ImageMagic extends SurfaceView implements Runnable{
     @Override
     public void onDraw(Canvas canvas) {
         canvas.drawBitmap(img, matrix, null);
+    }
+
+    private Bitmap reduceImageFast(Bitmap source, final int newWidth, final int newHeight) {
+        final int[] dx = {0, 1, 0, 1},
+                    dy = {0, 0, 1, 1};
+        final int N = 4;
+        Thread[] t = new Thread[N];
+
+        final int width = source.getWidth();
+        final int height = source.getHeight();
+        final int[] newRaw = new int[newHeight * newWidth],
+                raw = new int[width * height];
+        source.getPixels(raw, 0, width, 0, 0, width, height);
+
+        for (int i = 0; i < N; i++) {
+            final int finalI = i;
+            t[i] = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    int oldX, offset;
+                    for(int y = (newHeight >> 1) * dy[finalI]; y < (newHeight >> 1) * (dy[finalI] + 1); y++) {
+                        offset = (int)Math.ceil((height - 1) * y / newHeight) * width;
+                        for (int x = (newWidth >> 1) * dx[finalI]; x < (newWidth >> 1) * (dx[finalI] + 1); x++) {
+                            oldX = (int)Math.ceil((width - 1) * x / newWidth);
+                            newRaw[y * newWidth + x] = raw[offset + oldX];
+                        }
+                    }
+                }
+            });
+        }
+
+        for (int i = 0; i < N; i++)
+            t[i].start();
+        for (int i = 0; i < N; i++) {
+            try {
+                t[i].join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return Bitmap.createBitmap(newRaw, newWidth, newHeight, Bitmap.Config.ARGB_8888);
     }
 }
