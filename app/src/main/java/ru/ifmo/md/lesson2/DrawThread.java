4222be88c19a43315ca02e3b7f2ffa67c589453f
warrior_c <gerralizza@gmail.com>
2014-09-22 03:53:47 +0400

    Bilinear interpolation added

--- app/src/main/java/ru/ifmo/md/lesson2/DrawThread.java
+++ app/src/main/java/ru/ifmo/md/lesson2/DrawThread.java
@@ -7,6 +7,7 @@ import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Matrix;
+import android.graphics.Paint;
 import android.graphics.PixelFormat;
 import android.os.Build;
 import android.util.Log;
@@ -31,6 +32,8 @@ public class DrawThread extends Thread {
     public enum CompressType {STUPID, SMART};
     CompressType compressType;
 
+    Paint paint = null;
+
     public DrawThread(SurfaceHolder surfaceHolder, Resources resources) {
         this.surfaceHolder = surfaceHolder;
         picture = BitmapFactory.decodeResource(resources, R.drawable.source);
@@ -38,6 +41,9 @@ public class DrawThread extends Thread {
         height = picture.getHeight();
         pixels = new int[width * height];
         picture.getPixels(pixels, 0, width, 0, 0, width, height);
+        paint = new Paint();
+        paint.setColor(Color.RED);
+        paint.setTextSize(50);
 
         makeBrightUp();
         compressSmart(405, 434);
@@ -77,7 +83,10 @@ public class DrawThread extends Thread {
         picture = Bitmap.createBitmap(pixels, width, height, picture.getConfig());
         synchronized (surfaceHolder) {
             canvas.drawColor(Color.BLACK);
-            if (picture != null)canvas.drawBitmap(picture, 0, 0, null);
+            if (picture != null) {
+                canvas.drawBitmap(picture, 0, 0, null);
+                canvas.drawText(compressType.toString(), picture.getWidth() + 10, 100, paint);
+            }
         }
     }
 
@@ -88,9 +97,6 @@ public class DrawThread extends Thread {
             int g = (curPixel & 0x0000ff00) >> 8;
             int b = curPixel & 0x000000ff;
 
-//            r = r + 128;
-//            g = g + 128;
-//            b = b + 128;
             r *= 2;
             g *= 2;
             b *= 2;
@@ -119,30 +125,76 @@ public class DrawThread extends Thread {
         compressType = CompressType.STUPID;
     }
 
+//    public void compressSmart(int width, int height) {
+//        float scaleX = (float)this.width / (float)width;
+//        float scaleY = (float)this.height / (float)height;
+//        int [] newPixels = new int[width * height];
+//        float y1 = 0;
+//        for (int j = 0; j < height; j++, y1 += scaleY) {
+//            float x1 = 0;
+//            for (int i = 0; i < width; i++, x1 += scaleX) {
+//                int x2 = (int)Math.min(this.width, x1 + Math.ceil(scaleX));
+//                int y2 = (int)Math.min(this.height, y1 + Math.ceil(scaleY));
+//                int pixelsCnt = (x2 - (int)x1) * (y2 - (int)y1);
+//                int nr = 0, ng = 0, nb = 0;
+//                for (int x = (int)x1; x < x2; x++) {
+//                    for (int y = (int)y1; y < y2; y++) {
+//                        int curPixel = pixels[y * this.width + x];
+//                        nr += ((curPixel & 0x00ff0000) >> 16);
+//                        ng += ((curPixel & 0x0000ff00) >> 8);
+//                        nb += ( curPixel & 0x000000ff);
+//                    }
+//                }
+//                nr /= pixelsCnt;
+//                ng /= pixelsCnt;
+//                nb /= pixelsCnt;
+//                newPixels[j * width + i] = 0xff000000 | (nr << 16) | (ng << 8) | nb;
+//            }
+//        }
+//        this.width = width;
+//        this.height = height;
+//        pixels = newPixels;
+//        compressType = CompressType.SMART;
+//    }
+
+    float linear(float s, float e, float t) {
+        return s + (e - s) * t;
+    }
+    float bilinear(float c00, float c10, float c01, float c11, float tx, float ty){
+        return linear(linear(c00, c10, tx), linear(c01, c11, tx), ty);
+    }
+
+    // I used some parts from the http://rosettacode.org/wiki/Bilinear_interpolation
     public void compressSmart(int width, int height) {
-        float scaleX = (float)this.width / (float)width;
-        float scaleY = (float)this.height / (float)height;
         int [] newPixels = new int[width * height];
-        float y1 = 0;
-        for (int j = 0; j < height; j++, y1 += scaleY) {
-            float x1 = 0;
-            for (int i = 0; i < width; i++, x1 += scaleX) {
-                int x2 = (int)Math.min(this.width, x1 + Math.ceil(scaleX));
-                int y2 = (int)Math.min(this.height, y1 + Math.ceil(scaleY));
-                int pixelsCnt = (x2 - (int)x1) * (y2 - (int)y1);
-                int nr = 0, ng = 0, nb = 0;
-                for (int x = (int)x1; x < x2; x++) {
-                    for (int y = (int)y1; y < y2; y++) {
-                        int curPixel = pixels[y * this.width + x];
-                        nr += ((curPixel & 0x00ff0000) >> 16);
-                        ng += ((curPixel & 0x0000ff00) >> 8);
-                        nb += ( curPixel & 0x000000ff);
-                    }
-                }
-                nr /= pixelsCnt;
-                ng /= pixelsCnt;
-                nb /= pixelsCnt;
-                newPixels[j * width + i] = 0xff000000 | (nr << 16) | (ng << 8) | nb;
+
+        for(int x = 0, y = 0; y < height;){
+            float gx = x / (float)(width) * (this.width - 1);
+            float gy = y / (float)(height) * (this.height - 1);
+            int gxi = (int)gx;
+            int gyi = (int)gy;
+            int p00 = pixels[gyi * this.width + gxi];
+            int p10 = pixels[gyi * this.width + (gxi + 1)];
+            int p01 = p00;
+            if ((gyi + 1) * this.width + gxi < this.width * this.height) {
+                p01 = pixels[(gyi + 1) * this.width + gxi];
+            }
+            int p11 = p00;
+            if ((gyi + 1) * this.width + (gxi + 1) < this.width * this.height) {
+                p11 = pixels[(gyi + 1) * this.width + (gxi + 1)];
+            }
+            int result = 0xff000000;
+            for(int i = 0; i < 3; i++){
+                int c00 = (p00 >> (i * 8)) & 0xff;
+                int c10 = (p10 >> (i * 8)) & 0xff;
+                int c01 = (p01 >> (i * 8)) & 0xff;
+                int c11 = (p11 >> (i * 8)) & 0xff;
+                result |= (int)bilinear(c00, c10, c01, c11, gx - gxi, gy - gyi) << (8 * i);
+            }
+            newPixels[y * width + x] = result;
+            x++;
+            if(x >= width){
+                x = 0; y++;
             }
         }
         this.width = width;
