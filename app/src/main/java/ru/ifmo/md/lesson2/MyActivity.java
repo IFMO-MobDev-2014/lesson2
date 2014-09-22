bc21474088d8f195319877d0194e938c1fb990a7
pinguinson <nikita.penguin@gmail.com>
2014-09-22 16:08:00 +0400

    added resizeImage() and resize resizeBitmap() methods

--- app/src/main/java/ru/ifmo/md/lesson2/MyActivity.java
+++ app/src/main/java/ru/ifmo/md/lesson2/MyActivity.java
@@ -1,6 +1,8 @@
 package ru.ifmo.md.lesson2;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
@@ -10,6 +12,7 @@ public class MyActivity extends Activity {
 
     ImageView image;
     boolean setFast;
+    public static final double RATIO = 1.0 / 1.84;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -18,6 +21,7 @@ public class MyActivity extends Activity {
         setFast = true;
 
         image = (ImageView) findViewById(R.id.image);
+        resizeImage(image, RATIO);
     }
 
     public void changeImage(View view) {
@@ -25,8 +29,21 @@ public class MyActivity extends Activity {
             image.setImageResource(R.drawable.source2);
         } else {
             image.setImageResource(R.drawable.source);
+
         }
+        resizeImage(image, RATIO);
         setFast = !setFast;
+    }
+    public Bitmap resizeBitmap(Bitmap source, double ratio) {
+        int newWidth = (int) (source.getWidth() * ratio);
+        int newHeight = (int) (source.getHeight() * ratio);
+        return Bitmap.createScaledBitmap(source, newWidth, newHeight, false);
+    }
+
+    public void resizeImage(ImageView image, double ratio) {
+        Bitmap current = ((BitmapDrawable) image.getDrawable()).getBitmap();
+        Bitmap newBitmap = resizeBitmap(current, ratio);
+        image.setImageBitmap(newBitmap);
     }
 
 }
