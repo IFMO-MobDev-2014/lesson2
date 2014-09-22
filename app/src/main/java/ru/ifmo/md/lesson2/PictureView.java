373753e6895ee27976eaa8b22f6fa51320fcbfd3
Viktor <viteg34@yandex.ru>
2014-09-22 19:39:17 +0400

    add rotate

--- app/src/main/java/ru/ifmo/md/lesson2/PictureView.java
+++ app/src/main/java/ru/ifmo/md/lesson2/PictureView.java
@@ -6,11 +6,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
 import android.view.View;
-
-import java.lang.reflect.Array;
 
 /**
  * Created by vi34 on 20.09.14.
@@ -18,21 +14,18 @@ import java.lang.reflect.Array;
 public class PictureView extends View {
     private int width;
     private int height;
-    private int nwidth;
-    private int nheight;
     private int p_image[];
     private int image[];
     private int image2[];
     private Bitmap bmp;
     private Resources res = getResources();
-    double perc = 1.0;
     boolean step = true;
 
     public PictureView (Context context){
         super(context);
-        BitmapFactory.Options op = new BitmapFactory.Options();
-        op.inScaled = false;
-        bmp = BitmapFactory.decodeResource(res, R.drawable.source, op);
+        BitmapFactory.Options option = new BitmapFactory.Options();
+        option.inScaled = false;
+        bmp = BitmapFactory.decodeResource(res, R.drawable.source, option);
         width = bmp.getWidth();
         height = bmp.getHeight();
         image = new int[width * height];
@@ -40,9 +33,7 @@ public class PictureView extends View {
 
         for(int i = 0; i < 100; i++)
             image[100 + i] = Color.WHITE;
-        bmp.getPixels(image, 0, width, 0, 0, width,height);
-        System.arraycopy(image,0,image2,0,image.length);
-
+        bmp.getPixels(image, 0, width, 0, 0, width, height);
 
         OnClickListener listener = new OnClickListener() {
             @Override
@@ -59,45 +50,60 @@ public class PictureView extends View {
         };
 
         setOnClickListener(listener);
-        nwidth = (int)Math.ceil(width/1.73);
-        nheight = (int)Math.ceil(height/1.73);
-        image = fastScale(image,width,height,nwidth, nheight);
 
+
+
+        image = rotate(image);
         changeBrithness(image, 2.0);
+        System.arraycopy(image,0,image2,0,image.length);
+
+
+        image = fastScale(image,1.73, 1.73);
+        image2 = fastScale(image2,1.73, 1.73);
+        width = (int)Math.ceil(width/1.73);
+        height = (int)Math.ceil(height/1.73);
+
         p_image = image;
     }
 
     void changeBrithness(int array[], double percent){
-        for(int i = 0; i < array.length; ++i)
-        {
+        for(int i = 0; i < array.length; ++i){
             int alpha = Color.alpha(array[i]);
             int red = Color.red(array[i]);
             int green = Color.green(array[i]);
             int blue = Color.blue(array[i]);
-            red *= percent;
-            green *= percent;
-            blue *= percent;
-            if(red > 255)
-                red = 255;
-            if(green > 255)
-                green = 255;
-            if(blue > 255)
-                blue = 255;
+            red = (int)Math.min(red * percent, 255);
+            green =(int) Math.min(green * percent, 255);
+            blue = (int)Math.min(blue * percent, 255);
             array[i] = Color.argb(alpha,red,green,blue);
-
         }
     }
 
-    int[] fastScale(int array[],int oldWidth, int oldHeight, int newWidth, int newHeight) {
-        int[] nImage = new int[newWidth * newHeight];
-        double w_ratio = oldWidth / (double)newWidth;
-        double h_ratio = oldHeight / (double)newHeight;
+    int[] rotate(int array[]){
+
+        int[] narray = new int[array.length];
+        for(int i = 0; i < width; ++i){
+            for(int j = 0; j < height; ++j) {
+                narray[i * height + j] = array[width * (height - j - 1) + i];
+            }
+        }
+        int tmp = width;
+        width = height;
+        height = tmp;
+        return narray;
+    }
+
+    int[] fastScale(int array[],double w_scale, double h_scale) {
+        int nWidth = (int)Math.ceil(width/1.73);
+        int nHeight = (int)Math.ceil(height/1.73);
+
+        int[] nImage = new int[nWidth * nHeight];
         double px, py;
-        for(int i = 0; i < newHeight; i++){
-            for(int j = 0; j < newWidth; j++){
-                px = Math.floor(j * w_ratio) ;
-                py = Math.floor(i * h_ratio) ;
-                nImage[(i * newWidth) + j] = array[(int)((py * oldWidth)+px)] ;
+        for(int i = 0; i < nHeight; i++){
+            for(int j = 0; j < nWidth; j++){
+                px = Math.floor(j * w_scale) ;
+                py = Math.floor(i * h_scale) ;
+                nImage[(i * nWidth) + j] = array[(int)((py * width) + px)] ;
             }
 
         }
@@ -113,7 +119,7 @@ public class PictureView extends View {
 
     @Override
     protected void onDraw(Canvas canvas) {
-        canvas.drawBitmap(p_image,0,nwidth,0,0,nwidth,nheight,true,null);
+        canvas.drawBitmap(p_image,0,width,0,0,width,height,true,null);
     }
 }
 
