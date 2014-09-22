3d90230d3fee4fa72a774f854128548f5254c587
nststnchl <nststnchl@MacBook-Pro-Aleksej.local>
2014-09-22 23:55:32 +0400

    Finished

--- app/src/main/java/ru/ifmo/md/lesson2/MyActivity.java
+++ app/src/main/java/ru/ifmo/md/lesson2/MyActivity.java
@@ -2,11 +2,22 @@ package ru.ifmo.md.lesson2;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MyActivity extends Activity {
+
+    private MyImageView myImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        myImageView = new MyImageView(this);
+        setContentView(myImageView);
+        myImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                myImageView.modeChange();
+            }
+        });
     }
 }
