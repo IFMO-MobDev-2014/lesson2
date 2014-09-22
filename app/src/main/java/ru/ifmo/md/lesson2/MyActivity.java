7e306053d17fe5b4226203eba8a38664a5e70922
Belonogov Ivan <Belonogov95@gmail.com>
2014-09-22 21:47:13 +0400

    before

--- app/src/main/java/ru/ifmo/md/lesson2/MyActivity.java
+++ app/src/main/java/ru/ifmo/md/lesson2/MyActivity.java
@@ -1,14 +1,10 @@
 package ru.ifmo.md.lesson2;
 
 import android.app.Activity;
-import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
-import android.graphics.Rect;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.SurfaceHolder;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -27,8 +23,7 @@ public class MyActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.active);
         bitmap0 = BitmapFactory.decodeResource(getResources(), R.drawable.source);
-        Log.i("w h", "::::::::::::::  " + bitmap0.getWidth() + "   " + bitmap0.getHeight());
-        imageView = (ImageView)findViewById(R.id.imageView);
+        imageView = (ImageView)findViewById(R.id.ViewXXX);
         check();
     }
     void check() {
@@ -37,7 +32,6 @@ public class MyActivity extends Activity {
         else
             bad();
         imageView.setImageBitmap(bitmap);
-
     }
 
     void good() {
@@ -45,22 +39,11 @@ public class MyActivity extends Activity {
     }
 
     void bad() {
-        //bitmap = rotate90(compressionBad(bitmap0));
-        bitmap = rotate90(test());
+        bitmap = rotate90(compressionFast(bitmap0));
     }
 
-    public Bitmap test() {
-        Bitmap r = Bitmap.createBitmap(W, H, Bitmap.Config.ARGB_8888);
-        for (int i = 0; i < W; i++)
-            for (int j = 0; j < H; j++)
-                r.setPixel(i, j, 0xFF008080);
-        return r;
-    }
 
-    //public Bitmap compressionBad(Bitmap bitmap) {
-    //}
-
-    public Bitmap compression (Bitmap bitmap) {
+    public Bitmap compressionFast(Bitmap bitmap) {
         float scale = bitmap.getWidth() * 1f / W;
         Bitmap newBitmap = Bitmap.createBitmap(W, H, Bitmap.Config.ARGB_8888);
         int [] data = new int[bitmap.getHeight() * bitmap.getWidth()];
@@ -72,6 +55,54 @@ public class MyActivity extends Activity {
         for (int i = 0; i < W; i++)
             for (int j = 0; j < H; j++)
                 newData[j * W + i] = data[round(i * scale) + round(j * scale) * width];
+        for (int i = 0; i < W * H; i++) {
+            for (int k = 0; k < 3; k++) {
+                int cntBit = 8;
+                int tmp = (newData[i] >> (k * cntBit)) & ((1 << cntBit) - 1);
+                newData[i] ^= tmp << (k * cntBit);
+                tmp = min((1 << cntBit) - 1, tmp * 2);
+                newData[i] ^= tmp << (k * cntBit);
+            }
+        }
+        newBitmap.setPixels(newData, 0, W, 0, 0, W, H);
+        return newBitmap;
+    }
+
+    public Bitmap compression (Bitmap bitmap) {
+        float scale = bitmap.getWidth() * 1f / W;
+        Bitmap newBitmap = Bitmap.createBitmap(W, H, Bitmap.Config.ARGB_8888);
+        int [] data = new int[bitmap.getHeight() * bitmap.getWidth()];
+        int [] newData = new int [W * H];
+        int [] cnt = new int [W * H];
+        int [] r = new int[W * H];
+        int [] g = new int[W * H];
+        int [] b = new int[W * H];
+        int width = bitmap.getWidth();
+        int height = bitmap.getHeight();
+
+        bitmap.getPixels(data, 0, width, 0, 0, width, height);
+        int cntBit = 8;
+        for (int i = 0; i < W * H; i++)
+            r[i] = g[i] = b[i] = 0xFF;
+        for (int i = 0; i < width; i++)
+            for (int j = 0; j < height; j++) {
+                int from = i + j * width;
+                int to  = round(j / scale) * W + round(i / scale);
+                r[to] = min(r[to], data[from] & ((1 << cntBit) - 1));
+                g[to] = min(g[to], (data[from] >> cntBit) & ((1 << cntBit) - 1));
+                b[to] = min(b[to], (data[from] >> (cntBit * 2)) & ((1 << cntBit) - 1));
+//                cnt[to]++;
+//                r[to] += data[from] & ((1 << cntBit) - 1);
+//                g[to] += (data[from] >> cntBit) & ((1 << cntBit) - 1);
+//                b[to] += (data[from] >> (cntBit * 2)) & ((1 << cntBit) - 1);
+                //newData[j * W + i] = data[round(i * scale) + round(j * scale) * width];
+            }
+        for (int i = 0; i < W * H; i++) {
+            r[i] = min((1 << cntBit) - 1, r[i] * 2);
+            g[i] = min((1 << cntBit) - 1, g[i] * 2);
+            b[i] = min((1 << cntBit) - 1, b[i] * 2);
+            newData[i] = r[i] | (g[i] << cntBit) | (b[i] << (cntBit + cntBit)) | (0xFF << (cntBit * 3));
+        }
         newBitmap.setPixels(newData, 0, W, 0, 0, W, H);
         return newBitmap;
     }
@@ -81,15 +112,6 @@ public class MyActivity extends Activity {
         int [] data = new int[W * H];
         int [] newData = new int [W * H];
         bitmap.getPixels(data, 0, W, 0, 0, W, H);
-        for (int i = 0; i < W * H; i++) {
-            for (int k = 0; k < 3; k++) {
-                int cntBit = 8;
-                int tmp = (data[i] >> (k * cntBit)) & ((1 << cntBit) - 1);
-                data[i] ^= tmp << (k * cntBit);
-                tmp = min((1 << cntBit) - 1, tmp * 2);
-                data[i] ^= tmp << (k * cntBit);
-            }
-        }
         for (int i = 0; i < W; i++)
             for (int j = 0; j < H; j++)
                 newData[(W - i - 1) * H + j] = data[i + j * W];
@@ -103,6 +125,13 @@ public class MyActivity extends Activity {
     }
 
 
+//    public Bitmap test() {
+//        Bitmap r = Bitmap.createBitmap(W, H, Bitmap.Config.ARGB_8888);
+//        for (int i = 0; i < W; i++)
+//            for (int j = 0; j < H; j++)
+//                r.setPixel(i, j, 0xFF008080);
+//        return r;
+//    }
 //    public void run() {
 //    myDraw(canvas, bitmap);
 //   }
