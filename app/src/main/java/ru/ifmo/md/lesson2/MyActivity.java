f87f405c30681a690906de103b5cfe4e0a7d79f8
Maxim Slyusarenko <slyuslyu.m@mail.ru>
2014-09-22 18:46:24 +0400

    Final version

--- app/src/main/java/ru/ifmo/md/lesson2/MyActivity.java
+++ app/src/main/java/ru/ifmo/md/lesson2/MyActivity.java
@@ -5,8 +5,24 @@ import android.os.Bundle;
 
 public class MyActivity extends Activity {
 
+    Image image;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        image = new Image(this);
+        setContentView(image);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        image.resume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        image.pause();
     }
 }
