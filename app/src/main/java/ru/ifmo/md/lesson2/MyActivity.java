dc1bf6655a374673031b29c9be6508be2e2e87c3
Алексей Кацман, 2538 <Alex@MacBook-Pro-Aleksej.local>
2014-09-22 14:07:17 +0400

    Added brightening, rotating and fast compression

--- app/src/main/java/ru/ifmo/md/lesson2/MyActivity.java
+++ app/src/main/java/ru/ifmo/md/lesson2/MyActivity.java
@@ -8,5 +8,7 @@ public class MyActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(new ImageCompressor(this));
     }
+
 }
