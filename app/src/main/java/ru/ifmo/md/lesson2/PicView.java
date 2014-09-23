43b706b8f5287e191d78c98af4270c0b9f55f96f
KorolevaYana <koroleva1538@gmail.com>
2014-09-23 23:02:37 +0400

    Added fast and cool scaling

--- app/src/main/java/ru/ifmo/md/lesson2/PicView.java
+++ app/src/main/java/ru/ifmo/md/lesson2/PicView.java
@@ -15,11 +15,14 @@ public class PicView extends View {
     public static final int h = 750;
     public static final int w = 700;
 
-    public static final int newh = 434;
     public static final int neww = 405;
+    public static final int newh = 434;
+
+    //public static final float scale = 1.72811;
 
     Bitmap map;
     int[] img = new int[h * w];
+    int[] newimg = new int[neww * newh];
 
     public PicView(Context context) {
         super(context);
@@ -31,7 +34,7 @@ public class PicView extends View {
         int[] tmp = new int[h * w];
         for (int i = 0; i < w; i++) {
             for (int j = 0; j < h; j++) {
-                tmp[i* h + j] = img[(h - j - 1) * w + i];
+                tmp[i * h + j] = img[(h - j - 1) * w + i];
             }
         }
         img = tmp;
@@ -50,10 +53,50 @@ public class PicView extends View {
                 redv = (value & 0xFF0000) >> 16;
                 greenv = (value & 0x00FF00) >> 8;
                 bluev = (value & 0x0000FF);
+
                 redv = Math.min(redv * 2, 255);
                 greenv = Math.min(greenv * 2, 255);
                 bluev = Math.min(bluev * 2, 255);
-                img[i * w + j] = (redv << 16) + (greenv << 8) + bluev;
+
+                img[i * w + j] = (redv << 16) | (greenv << 8) | bluev;
+            }
+        }
+    }
+
+    public void fastScaling() {
+        float scale = w / (float)neww;
+        for (int i = 0; i < neww; i++) {
+            for (int j = 0; j < newh; j++) {
+                newimg[i * newh + j] = img[((int)(i * scale) * h + (int)(j * scale))];
+            }
+        }
+    }
+
+    public void coolScaling() {
+        float scale = h / (float)newh;
+
+        int[] redv = new int[newh * neww];
+        int[] greenv = new int[newh * neww];
+        int[] bluev = new int[newh * neww];
+        int[] cnt = new int[newh * neww];
+
+        for (int i = 0; i < w; i++) {
+            for (int j = 0; j < h; j++) {
+                int old_coord = i * h + j;
+                int new_coord = (int) ((float)i / scale) * newh + (int) ((float)j / scale);
+
+                redv[new_coord] += (img[old_coord] & 0xFF0000) >> 16;
+                greenv[new_coord] += (img[old_coord] & 0x00FF00) >> 8;
+                bluev[new_coord] += (img[old_coord] & 0x0000FF);
+                cnt[new_coord]++;
+            }
+        }
+        for (int i = 0; i < neww; i++) {
+            for (int j = 0; j < newh; j++) {
+                int coord = i * newh + j;
+                newimg[coord] = ((redv[coord] / cnt[coord]) << 16)
+                                | ((greenv[coord] / cnt[coord]) << 8)
+                                | (bluev[coord] / cnt[coord]);
             }
         }
     }
@@ -63,7 +106,10 @@ public class PicView extends View {
     public void onDraw(Canvas canvas) {
         upBrightness();
         rotate();
-        canvas.drawBitmap(img, 0, h, 0, 0, h, w, false, null);
+        //fastScaling();
+        coolScaling();
+        canvas.drawBitmap(newimg, 0, newh, 0, 0, newh, neww, false, null);
+        //canvas.drawBitmap(img, 0, h, 0, neww, h, w, false, null);
     }
 
 }
