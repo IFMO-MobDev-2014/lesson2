c0090db8f8ba213f28d23aea1c89802611531d2d
snitch163 <prorochek@gmail.com>
2014-09-24 23:47:50 +0400

    Revert "[+] code"
    This reverts commit dfd6038f598f71a3921c3167cfacb26023ca23a9.

--- app/src/main/java/ru/ifmo/md/lesson2/MyActivity.java
+++ app/src/main/java/ru/ifmo/md/lesson2/MyActivity.java
@@ -1,25 +1,12 @@
 package ru.ifmo.md.lesson2;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.os.Bundle;
 import android.app.Activity;
-import android.widget.ImageView;
-import java.lang.Override;
+import android.os.Bundle;
 
-public class MyActivity extends Activity{
-
-
+public class MyActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ImageView rot;
-        setContentView(R.layout.activity);
-        rot = (ImageView)findViewById(R.id.RotatorAndShrinker);
-        rot.setScaleType(ImageView.ScaleType.CENTER);
-        Bitmap bit = BitmapFactory.decodeResource(this.getResources(), R.drawable.source, new BitmapFactory.Options());
-        rot.setImageBitmap(bit);
     }
-
 }
