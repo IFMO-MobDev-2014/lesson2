366e29efec6fd7c8b6d696a63a4f60e3d3ac0cfd
sid364 <sid364@gmail.com>
2014-09-29 23:08:19 +0400

    all done

--- app/src/main/java/ru/ifmo/md/lesson2/MyActivity.java
+++ app/src/main/java/ru/ifmo/md/lesson2/MyActivity.java
@@ -2,11 +2,36 @@ package ru.ifmo.md.lesson2;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MyActivity extends Activity {
 
+    private MySurfaceView mySurfaceView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mySurfaceView = new MySurfaceView(this);
+        setContentView(mySurfaceView);
+        mySurfaceView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                mySurfaceView.Change();
+                return false;
+            }
+        });
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        mySurfaceView.resume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mySurfaceView.pause();
     }
 }
