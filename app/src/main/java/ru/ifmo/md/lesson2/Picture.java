c12c198b013e0e633a554494be0163f8b5c4e20e
kirkutirev <kutirev-k@mail.ru>
2014-10-05 12:11:58 +0700

    begining

---
+++ app/src/main/java/ru/ifmo/md/lesson2/Picture.java
@@ -0,0 +1,13 @@
+package ru.ifmo.md.lesson2;
+
+import android.app.Activity;
+/**
+ * Created by Kirill on 04.10.2014.
+ */
+public class Picture {
+    private final int baseWidth = 700;
+    private final int baseHeight = 750;
+    private final int targetWidth = 405;
+    private final int targetHeight = 434;
+
+}
