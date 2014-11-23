3bf1238179b4cc8960f75e829a527b8283f4adf3
Vlad <vlad007.1996@yandex.ru>
2014-11-23 17:06:09 +0300

    Add code.

--- app/src/main/java/ru/ifmo/md/lesson2/MyActivity.java
+++ app/src/main/java/ru/ifmo/md/lesson2/MyActivity.java
@@ -2,11 +2,35 @@ package ru.ifmo.md.lesson2;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 
 public class MyActivity extends Activity {
 
+    private PhotoView photoView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        photoView = new PhotoView(this);
+        setContentView(photoView);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        photoView.resume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        photoView.pause();
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if(event.getAction() == MotionEvent.ACTION_DOWN) {
+            photoView.changeQuality();
+        }
+        return true;
     }
 }
