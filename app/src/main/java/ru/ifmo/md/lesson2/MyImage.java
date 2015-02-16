d6e0e8c245fcfb15d474d3290c144b3e1b42cbb9
FluffyBear <maksimenkoaleksan96@yandex.ru>
2015-02-16 14:55:34 +0300

    Fixed it + added apk

--- app/src/main/java/ru/ifmo/md/lesson2/MyImage.java
+++ app/src/main/java/ru/ifmo/md/lesson2/MyImage.java
@@ -8,92 +8,95 @@ import android.graphics.Color;
 import android.graphics.Matrix;
 import android.view.View;
 
-public class MyImage extends View {
-    Matrix m = new Matrix();
-    Bitmap bitmap;
-    Bitmap bitmap2;
-    boolean flag = true;
 
+public class MyImage extends View {
+    BitmapFactory.Options options = new BitmapFactory.Options();
+    Bitmap bitmap, initBitmap;
+    Matrix m = new Matrix();
+    boolean state;
+    float factor = 1.73f;
 
     public MyImage(Context context) {
         super(context);
         init();
         this.setOnClickListener(new OnClickListener() {
             public void onClick(View v) {
-                bitmap2 = flag ? QualityScale(bitmap) : FastScale(bitmap);
-                flag = !flag;
+                bitmap = state ? beautyScale(initBitmap, factor) : fastScale(initBitmap, factor);
+                state = !state;
                 invalidate();
             }
         });
     }
 
     private void init() {
-        bitmap = RotateCW(BitmapFactory.decodeResource(getResources(), R.drawable.source)).copy(Bitmap.Config.ARGB_8888, true);
-        bitmap = (Brightness(bitmap));
-        bitmap2 = FastScale(bitmap);
+        options.inScaled = false;
+        bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.source, options);
+        initBitmap = bright(rotate(bitmap));
+        bitmap = fastScale(initBitmap, factor);
     }
 
     public void resume() {
         invalidate();
     }
 
-    Bitmap RotateCW(Bitmap b) {
-        int width = b.getWidth();
-        int height = b.getHeight();
-        int[] pixels = new int[width * height];
-        b.getPixels(pixels, 0, width, 0, 0, width, height);
-        int[] newColors = new int[width * height];
-        for (int y = 0; y < width; y++) {
-            for (int x = 0; x < height; x++) {
-                newColors[y * height + x] = pixels[y + (height - 1 - x) * width];
+    Bitmap rotate(Bitmap b) {
+        int w = b.getWidth();
+        int h = b.getHeight();
+        int[] c = new int[w * h];
+        b.getPixels(c, 0, w, 0, 0, w, h);
+        int[] newColors = new int[w * h];
+        for (int y = 0; y < w; y++) {
+            for (int x = 0; x < h; x++) {
+                newColors[y * h + x] = c[y + (h - 1 - x) * w];
             }
         }
-        return Bitmap.createBitmap(newColors, height, width, Bitmap.Config.ARGB_8888);
-    }
-    Bitmap Brightness(Bitmap b) {
-        int width = b.getWidth();
-        int height = b.getHeight();
-        int[] pixels = new int[width * height];
-        b.getPixels(pixels, 0, width, 0, 0, width, height);
-        for (int i = 0; i < width * height; i++) {
-            int white = 0xff;
-            int newRed = (((pixels[i] >> 8 * 2) & white) + white) >> 1;
-            int newGreen = (((pixels[i] >> 8) & white) + white) >> 1;
-            int newBlue = ((pixels[i] & white) + white) >> 1;
-            pixels[i] = (white << 8 * 3) | (newRed << 8 * 2) | (newGreen << 8) | newBlue;
-        }
-        return Bitmap.createBitmap(pixels, width, height, Bitmap.Config.ARGB_8888);
+        return Bitmap.createBitmap(newColors, h, w, Bitmap.Config.ARGB_8888);
     }
 
-    Bitmap FastScale(Bitmap b) {
-        int intFactor = (int) (1.73f * 100);
-        int width = b.getWidth();
-        int height = b.getHeight();
-        int[] pixels = new int[width * height];
-        bitmap.getPixels(pixels, 0, width, 0, 0, width, height);
-        int newWidth = width * 100 / intFactor;
-        int newHeight = height * 100 / intFactor;
+    Bitmap bright(Bitmap b) {
+        int w = b.getWidth();
+        int h = b.getHeight();
+        int[] c = new int[w * h];
+        b.getPixels(c, 0, w, 0, 0, w, h);
+        for (int i = 0; i < w * h; i++) {
+            int white = 0xff;
+            int newRed = (((c[i] >> 8 * 2) & white) + white) >> 1;
+            int newGreen = (((c[i] >> 8) & white) + white) >> 1;
+            int newBlue = ((c[i] & white) + white) >> 1;
+            c[i] = (white << 8 * 3) | (newRed << 8 * 2) | (newGreen << 8) | newBlue;
+        }
+        return Bitmap.createBitmap(c, w, h, Bitmap.Config.ARGB_8888);
+    }
+
+    Bitmap fastScale(Bitmap b, float factor) {
+        int intFactor = (int) (factor * 100);
+        int w = b.getWidth();
+        int h = b.getHeight();
+        int[] c = new int[w * h];
+        b.getPixels(c, 0, w, 0, 0, w, h);
+        int newWidth = w * 100 / intFactor;
+        int newHeight = h * 100 / intFactor;
         int[] newColors = new int[newWidth * newHeight];
         for (int y = 0; y < newHeight; y++) {
             for (int x = 0; x < newWidth; x++) {
-                newColors[y * newWidth + x] = pixels[(y * intFactor / 100) * width + (x * intFactor / 100)];
+                newColors[y * newWidth + x] = c[(y * intFactor / 100) * w + (x * intFactor / 100)];
             }
         }
         return Bitmap.createBitmap(newColors, newWidth, newHeight, Bitmap.Config.ARGB_8888);
     }
 
-    Bitmap QualityScale(Bitmap b) {
-        int width = b.getWidth();
-        int height = b.getHeight();
-        int New_Width = (int) (width / 1.73f);
-        int New_Height = (int) (height / 1.73f);
-        int[] newColors = new int[New_Width * New_Height];
-        for (int y = 0; y < New_Height; y++) {
-            for (int x = 0; x < New_Width; x++) {
-                int x1 = (int) (x * 1.73f);
-                int y1 = (int) (y * 1.73f);
-                int x2 = (int) ((x + 1) * 1.73f - 0.01f);
-                int y2 = (int) ((y + 1) * 1.73f - 0.01f);
+    Bitmap beautyScale(Bitmap b, float factor) {
+        int w = b.getWidth();
+        int h = b.getHeight();
+        int nw = (int) (w / factor);
+        int nh = (int) (h / factor);
+        int[] newColors = new int[nw * nh];
+        for (int y = 0; y < nh; y++) {
+            for (int x = 0; x < nw; x++) {
+                int x1 = (int) (x * factor);
+                int y1 = (int) (y * factor);
+                int x2 = (int) ((x + 1) * factor - 0.01f);
+                int y2 = (int) ((y + 1) * factor - 0.01f);
                 int dx = x2 - x1 + 1;
                 int dy = y2 - y1 + 1;
                 int[] c = new int[dx * dy];
@@ -111,13 +114,14 @@ public class MyImage extends View {
                     green += Color.green(c[i]);
                     blue += Color.blue(c[i]);
                 }
-                newColors[y * New_Width + x] = Color.rgb(red / k, green / k, blue / k);
+                newColors[y * nw + x] = Color.rgb(red / k, green / k, blue / k);
             }
         }
-        return Bitmap.createBitmap(newColors, New_Width, New_Height, Bitmap.Config.ARGB_8888);
+        return Bitmap.createBitmap(newColors, nw, nh, Bitmap.Config.ARGB_8888);
     }
+
     @Override
     public void onDraw(Canvas canvas) {
-        canvas.drawBitmap(bitmap2, m, null);
+        canvas.drawBitmap(bitmap, m, null);
     }
 }
